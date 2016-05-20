9bb5a9788db830b6bc9564792489473e25f5f2e8
Maxime LUCE <maxime@touchify.co>
2016-05-20 21:21:00 +0200

    Import samples from source repository to test typings

---
+++ test/samples/tablequerysample.ts
@@ -0,0 +1,165 @@
+﻿import * as fs from "fs";
+import * as assert from "assert";
+import * as util from "util";
+import * as http from "http";
+import * as azure from "azure-storage";
+
+import TableQuery = azure.TableQuery;
+import TableUtilities = azure.TableUtilities;
+import eg = TableUtilities.entityGenerator;
+
+var tableName = 'tablequerysample';
+var tableService = azure.createTableService();
+
+// optionally set a proxy
+/*var proxy = {
+  protocol: 'http:',
+  host: '127.0.0.1',
+  port: 8888
+};
+
+tableService.setProxy(proxy);*/
+
+export interface EntityType extends azure.TableService.TableEntity {
+  integerfield?: eg.EntityProperty<number>; 
+  stringfield?: eg.EntityProperty<number>; 
+  longfield?: eg.EntityProperty<number>; 
+}
+
+var entity1 = {
+  PartitionKey: eg.String('partition1'),
+  RowKey: eg.String('row1'),
+  integerfield: eg.Int32(1),
+  stringfield: eg.String('stringfield value'),
+  longfield: eg.Int64('92233720368547758')
+};
+
+var entity2 = {
+  PartitionKey: eg.String('partition1'),
+  RowKey: eg.String('row2'),
+  stringfield: eg.String('stringfield value'),
+  longfield: eg.Int64('8547758')
+};
+
+function performTableQuery() {
+  // Create the table
+  tableService.createTable(tableName, function (error1) {
+    assert.equal(error1, null);
+
+    // Insert the entities
+    insertEntities(function() {
+
+      // Return all entities
+      queryAllEntities(function () {
+
+        // Return entities Where certain conditions are met
+        queryEntitiesWhere(function () {
+
+          // Return the Top n entities
+          queryEntitiesTop(function () {
+
+            // Return Select fields from entities
+            queryEntitiesSelect(function () {
+
+              // Delete the table
+              tableService.deleteTable(tableName, function (error2) {
+                assert.equal(error2, null);
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+}
+
+function insertEntities(callback) {
+  // insert the entities
+  tableService.insertEntity(tableName, entity1, function (error1) {
+    assert.equal(error1, null);
+    tableService.insertEntity(tableName, entity2, function (error2) {
+      assert.equal(error2, null);
+      callback();
+    });
+  });
+}
+
+function queryAllEntities(callback) {
+  // Select all fields
+  tableService.queryEntities<EntityType>(tableName, null, null, function (error, result) {
+    assert.equal(error, null);
+    assert.notEqual(result, null);
+    assert.notEqual(result.entries, null);
+
+    var entities = result.entries;
+    assert.equal(entities.length, 2);
+    var entityResult = entities[0];
+    assert.equal(entityResult.stringfield._, entity1.stringfield._);
+    assert.equal(entityResult.longfield._, entity1.longfield._);
+
+    callback();
+  });
+}
+
+function queryEntitiesWhere(callback) {
+  // Select only the entries where the longfield is great than 10,000,000
+
+  // equivalently: var tableQuery = new TableQuery().where('longfield == ?int64?', '10000000');
+  var tableQuery = new TableQuery().where(TableQuery.int64Filter('longfield', TableUtilities.QueryComparisons.GREATER_THAN, '10000000'));
+
+  tableService.queryEntities<EntityType>(tableName, tableQuery, null, function (error, result) {
+    assert.equal(error, null);
+    assert.notEqual(result, null);
+    assert.notEqual(result.entries, null);
+
+    var entities = result.entries;
+    assert.equal(entities.length, 1);
+
+    var entityResult = entities[0];
+    assert.equal(entityResult.longfield._, entity1.longfield._);
+
+    callback();
+  });
+}
+
+function queryEntitiesTop(callback) {
+  // Select only the top entry
+  var tableQuery = new TableQuery().top(1);
+
+  tableService.queryEntities<EntityType>(tableName, tableQuery, null, function (error, result) {
+    assert.equal(error, null);
+    assert.notEqual(result, null);
+    assert.notEqual(result.entries, null);
+
+    var entities = result.entries;
+    assert.equal(entities.length, 1);
+
+    var entityResult = entities[0];
+    assert.equal(entityResult.integerfield._, entity1.integerfield._);
+    assert.equal(entityResult.longfield._, entity1.longfield._);
+
+    callback();
+  });
+}
+
+function queryEntitiesSelect(callback) {
+  // Select specific field
+  var tableQuery = new TableQuery().select('integerfield');
+
+  tableService.queryEntities<EntityType>(tableName, tableQuery, null, function (error, result) {
+    assert.equal(error, null);
+    assert.notEqual(result, null);
+    assert.notEqual(result.entries, null);
+
+    var entities = result.entries;
+    assert.equal(entities.length, 2);
+
+    var entityResult = entities[0];
+    assert.equal(entityResult.integerfield._, entity1.integerfield._);
+    assert.equal(entityResult.longfield, undefined);
+
+    callback();
+  });
+}
+
+performTableQuery();
